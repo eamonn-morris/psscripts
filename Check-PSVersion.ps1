827cef0825b63d6786fb47ef7e177dee75d07fc5
Eamonn Morris <eamonnmorris@protonmail.com>
2024-08-12 22:03:20 -0500

    changed function name to approved verb

--- Check-PSVersion.ps1
+++ Check-PSVersion.ps1
@@ -2,15 +2,15 @@
 # Snippet to verify that a script is running under PS 7 vs. 5
 #
 
-function Check-PSVersion {
-	if ($PSVersionTable.PSVersion.Major -lt 7) {
-		Write-Warning "This script requires PowerShell 7 or higher. You are running version $($PSVersionTable.PSVersion)."
-		$confirmation = Read-Host "Do you want to continue anyway? (y/n)"
-		if ($confirmation -ne 'y') {
-			Write-Output "Exiting script"
-			# return 1
-		}
-	}
+function Confirm-PSVersion {
+    if ($PSVersionTable.PSVersion.Major -lt 7) {
+        Write-Warning "This script requires PowerShell 7 or higher. You are running version $($PSVersionTable.PSVersion)."
+        $confirmation = Read-Host "Do you want to continue anyway? (y/n)"
+        if ($confirmation -ne 'y') {
+            Write-Output "Exiting script"
+            # return 1
+        }
+    }
 }
 
-Check-PSVersion
+Confirm-PSVersion
